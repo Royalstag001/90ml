e1c300ea916e7f7b24edbde429ea9fa352fc4e8b
Royalstag001 <root@master.example.com>
2025-07-24 11:43:23 +0530

    terraform file

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "my_vm" {
+    ami = var.my_ami
+    instance_type = var.my_type
+
+    tags = {
+      Name = "Suraj_vm"
+    }
+  
+}
+
+resource "aws_security_group" "my_sg" {
+    name = "my_group"
+   description = "this is my new group"
+
+   tags = {
+     Name = "Suraj_sg"
+   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "my_rule" {
+  security_group_id = aws_security_group.my_sg.id
+  ip_protocol = "tcp"
+  cidr_ipv4 = var.my_ingress
+  from_port = var.ssh_port
+  to_port = var.ssh_port
+
+  tags = {
+    Name = "INBOUND_RULE"
+  }
+}
---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "my_ami" {
+  description = "allocated to us-east-1 region"
+}
+
+variable "my_type" {
+  description = "allocated to us-east-1 region"
+}
+
+variable "my_ingress" {
+  description = "my inbound rule"
+}
+
+variable "ssh_port" {
+  description = "port available for this vm"
+}
